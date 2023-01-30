80baddd4218525eb36bb67bd80ef63c0c4014553
Han Chen <han.chen795@gmail.com>
2023-01-29 17:33:30 -0800

    Initial commit

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -3,11 +3,12 @@ from scipy.spatial.distance import cdist
 
 
 class Silhouette:
-    def __init__(self):
+    def __init__(self, metric: str = "euclidean" ):
         """
         inputs:
             none
         """
+        self._metric = metric
 
     def score(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
         """
@@ -24,3 +25,48 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+
+        # X = matrix
+        # Y = labels
+        # Return array of scores
+
+        num_obs = X.shape[0] # Number of observations in X 
+        cluster_list = np.unique(y) # get the unique clusters
+        num_clusters = len(cluster_list) # number of clusters
+
+        # Calculate distance between all pairs of obs
+        pairwise_dists = cdist(X, X, metric = self._metric)
+        sil_scores = np.zeros(num_obs)
+
+        # loop through observations
+        for obs in range(0, num_obs):
+            current_cluster = y[obs] # Get current cluster
+            cluster_size = len(y[y == current_cluster]) # Get num of obs in cluster
+
+            if cluster_size == 1:
+                sil_scores[obs] = 0
+            else:
+                # Calculate a
+                a = self._calculate_a(pairwise_dists, y, current_cluster, obs)
+            
+                # Calculate b
+                b = self._calculate_b(pairwise_dists, y, current_cluster, obs, cluster_list)
+                sil_scores[obs] = (b-a)/max(a,b)        
+
+        return sil_scores
+
+    def _calculate_a(self, pariwise_dists, y, current_cluster, obs):
+        same_cluster_dists = pariwise_dists[y == current_cluster][:,obs]
+        a = np.mean(same_cluster_dists)
+        return a
+
+    def _calculate_b(self, pairwise_dists, y, current_cluster, obs, cluster_list):
+        other_clusters = np.delete(cluster_list, current_cluster)
+        b = np.inf
+        for c in other_clusters:
+            other_cluster_dists = pairwise_dists[y == c][:, obs]
+            _b = np.mean(other_cluster_dists)
+            b = min(b, _b)
+        return b
+
+    
--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -1,1 +1,53 @@
 # write your silhouette score unit tests here
+import pytest
+from cluster import kmeans, silhouette, utils
+import numpy as np
+import random
+import math
+from sklearn.metrics import silhouette_score
+
+# Test to make sure the silhouette score is between -1 and 1
+def kmean_example():
+    N = 500
+    mat, _ = utils.make_clusters(n = N, k = 3)
+
+    # Split the data into training and testing sets
+    idx = list(range(0,N))
+    train_idx = random.sample(idx, math.floor(N/4)) # Training set
+    test_idx = list(set(idx) - set(train_idx)) # Testing set
+
+    # Get training mat and test mat
+    mat_train = mat[train_idx, :]
+    mat_test = mat[test_idx, :]
+
+    # Initialize kmeans and train on training data
+    km = kmeans.KMeans(k = 3)
+
+    return(km, mat_test, mat_train)
+
+def test_silhouette_range():
+    km, mat_test, mat_train = kmean_example()
+    km.fit(mat_train)
+    # Get predicted labels of the test set
+    labels_pred = km.predict(mat_test)
+
+    # Calculate silhouette scores
+    sil = silhouette.Silhouette()
+    sil_scores = sil.score(mat_test, labels_pred)
+
+    assert (sil_scores.all() >= -1) and (sil_scores.all() <= 1)
+
+def test_compare_to_sklean():
+    km, mat_test, mat_train = kmean_example()
+    km.fit(mat_train)
+    # Get predicted labels of the test set
+    labels_pred = km.predict(mat_test)
+
+    # Calculate silhouette scores
+    sil = silhouette.Silhouette()
+
+    sil_scores = sil.score(mat_test, labels_pred)
+    sklearn_scores = silhouette_score(mat_test, labels_pred)
+    assert abs(np.mean(sil_scores) - sklearn_scores) < 0.01
+
+
